dd18b230b33c3dbd8345ce8007e836cc4ac39260
Beltium <97401253+Beltium@users.noreply.github.com>
2024-08-13 23:59:45 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def heartFunction(x, k):
-    f1 = np.cbrt(x)**2
-    f2 = 0.9 * ((3.3 - x ** 2) ** (1 / 2))
-    f3 = np.sin(np.pi * x * k)
-    return f1 + f2 * f3
-
-print(heartFunction(-1.5, 6.5))
